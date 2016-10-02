6bc1325e5eec025fde1882a1dda34f919ccf2fec
Don Kingery <kingery1@suddenlink.net>
2016-10-02 17:36:31 -0400

    ex34_plus

---
+++ ex34.rb
@@ -0,0 +1,16 @@
+ animals = ['bear', 'tiger', 'penquin', 'zebra']
+
+puts animals
+print animals
+print animals.length()
+puts "_________\n"
+puts animals[0]
+puts animals[-1]
+puts "---------\n"
+puts animals[0..2]
+
+print animals.push("Wild")
+puts "_________\n"
+print animals
+puts animals.reverse
+
