669392476b27fb65563ff298f1a60bf09c0ac933
Don Kingery <kingery1@suddenlink.net>
2016-10-05 12:51:35 -0400

    reworked ex34

--- ex33my_xtra_wk.rb
+++ ex33my_xtra_wk.rb
@@ -1,17 +1,17 @@
 i = 0
-even_no_members = []
+even_num_members = []
 
 while i < 20
 	puts "New Member ID number: #{i}"
-	even_no_members.push(i)
+	even_num_members.push(i)
 
 	i += 2
 	puts "Now for Next Even Mem No.:"
 	puts "Going to: #{i} "
 end
 
-puts "EVEN MEMBER NUMBERS"
+puts "EVEN NUMBER MEMBERS"
 
 puts "_____________another way_______"
 
-even_no_members.each {|num| puts num}
+even_num_members.each {|num| puts num}
--- ex34.rb
+++ ex34.rb
@@ -1,3 +1,41 @@
+#ex35
+puts " Ok; here is home work"
+
+
+animals = ['1st_bear', '2nd_ruby', '3rd_peacock', '4th_kangaroo', '5th_whale', '6th_playtipus']
+
+print animals
+
+puts "_____here is list of animals_________"
+
+x = animals
+
+print "The animal at 1 ................."
+puts x[1]
+print "The third (3rd) animal............"
+puts x[2]
+print "The first (1st) animal.............."
+puts x[0]
+print "the animal at 3...................."
+puts x[3]
+print "The fifth (5th) animal............."
+puts x[4]
+print "The animal at 3................."
+puts x[3]
+print "the animal at 2.................."
+puts x[2]
+print "The sixth (6th) animal..........."
+puts x[-1]
+print "The animal at 4................"
+puts x[3]
+
+
+
+
+
+
+puts "OK_______below is my stuff_________"
+
  animals = ['bear', 'tiger', 'penquin', 'zebra']
 
 puts animals
