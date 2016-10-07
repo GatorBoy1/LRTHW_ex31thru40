28fae0e2dc1f469aa3d1bd94e0b9cb86144a8e6d
Don Kingery <kingery1@suddenlink.net>
2016-10-07 15:30:52 -0400

    ex37_38

---
+++ ex37.rb
@@ -0,0 +1,23 @@
+#Study Exercise
+#put out a piece of code using each item
+#in exercise 37
+
+study_array = ["Keywords", "Data_Types,", "String_Escape_Sequences", "Operators"]
+puts study_array
+
+puts "KEYWORDS:"
+
+
+
+
+puts "DATA TYPES"
+
+
+
+
+puts "STRING ESCAPE SEQUENCES"
+
+
+
+
+puts "OPERATORS"
